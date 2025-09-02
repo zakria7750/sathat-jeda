b1d9787b066831bf89aa090c61b561b0bd857884
Zakria Ahmed <za775012309@gmail.com>
2025-09-02 10:00:28 -0700

    as

--- server/index.ts
+++ server/index.ts
@@ -63,8 +63,7 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "127.0.0.1",
   }, () => {
     log(`serving on port ${port}`);
   });
